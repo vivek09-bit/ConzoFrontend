8ca715166834d8eadc23aeab2f2d4355c187b5ea
vivek <vkg1257@gmail.com>
2025-06-21 22:38:16 +0530

    working checked

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
+  preview: {
+    host: true,
+    allowedHosts: ['conzofrontend.onrender.com', 'asthica.com'],
+  },
 });
